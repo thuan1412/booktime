a1e3b6547011a04313c12c0897b3aa3f3958c954
thuan.dh <thuan14121999@gmail.com>
2019-11-27 23:30:36 +0700

    finish chap2 | create form | send mail

--- booktime/urls.py
+++ booktime/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name='home.html')),
+    path('', include('main.urls'))
+    # path("", TemplateView.as_view(template_name='home.html')),
+    # path('about-us/', TemplateView.as_view(template_name='about_us.html'))
 ]
