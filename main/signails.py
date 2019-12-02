e93c870f37b8b3c3af4b80d41584d2b609cdfc75
thuan.dh <thuan14121999@gmail.com>
2019-12-02 22:53:15 +0700

    add thumbnail for image field

--- main/signails.py
+++ main/signails.py
@@ -10,11 +10,23 @@ THUMBNAIL_SIZE = (300, 300)
 
 logger = logging.getLogger(__name__)
 
+
 @receiver(pre_save, sender=ProductImage)
 def generate_thumbnail(sender, instance, **kwargs):
     logger.info(
-        "Generating thumbail for product %d",
+        "Generating thumbnail for product %d",
         instance.product.id
     )
     image = Image.open(instance.image)
     image = image.convert("RGB")
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+    temp_thumb = BytesIO()
+    image.save(temp_thumb, "JPEG")
+    temp_thumb.seek(0)
+    instance.thumbnail.save(
+        instance.image.name,
+        ContentFile(temp_thumb.read()),
+        save=False,
+    )
+    temp_thumb.close()
