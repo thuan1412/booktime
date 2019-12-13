09c3c13d8196d13d9583f534f94241a0babc908b
thuan.dh <thuan14121999@gmail.com>
2019-12-14 00:16:12 +0700

    101

--- main/views.py
+++ main/views.py
@@ -16,7 +16,7 @@ class ContactUsView(FormView):
 
 
 class ProductListView(ListView):
-    template_name = "main/product_list.html"
+    template_name = "product_list.html"
     paginate_by = 4
 
     def get_queryset(self):
@@ -24,9 +24,11 @@ class ProductListView(ListView):
         self.tag = None
         if tag != "all":
             self.tag = get_object_or_404(
-                models.ProductTag, slug=slug)
+                models.ProductTag, slug=tag)
+
+        if self.tag:
+            products = models.Product.objects.active().filter(tags=self.tag)
         else:
             products = models.Product.objects.active()
-        
+
         return products.order_by("name")
-        
